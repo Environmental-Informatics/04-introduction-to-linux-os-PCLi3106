5b23c449c07038c76eacf2115ba960c8030850ff
Pin-Ching Li <li3106@scholar-fe02.rcac.purdue.edu>
2020-02-28 11:59:27 -0500

    git add Lab04

---
+++ Lab04/my_backup.sh
@@ -0,0 +1,12 @@
+"""
+Header Comments
+File Created on 02/21/2020 by Pin-Ching Li for ABE65100
+
+This script is written for backing up the things in current folder
+
+Everything would be copyed to the 'Backup' folder
+"""
+# Create a Backup folder for backup
+mkdir /home/li3106/ABE65100/Backup
+# copy everything to Backup folder
+cp -a /home/li3106/04-introduction-to-linux-os-PCLi3106 /home/li3106/ABE65100/Backup
